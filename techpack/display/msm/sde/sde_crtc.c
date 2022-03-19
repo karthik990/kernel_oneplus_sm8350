abb1ce6530cfd840847c149b302a515114ec6906
alk3pInjection <webmaster@raspii.tech>
2022-03-19 16:42:16 +0530

    disp: msm: Allow hooking dim when screen goes on
    Change-Id: I5ca0b3905ebfb7200cd04bfae28ad1feb06d1a44
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>

--- techpack/display/msm/sde/sde_crtc.c
+++ techpack/display/msm/sde/sde_crtc.c
@@ -2870,8 +2870,8 @@ ssize_t notify_fppress_store(struct device *dev,
 }
 
 extern int aod_layer_hide;
-extern int oneplus_panel_status;
 extern int backup_dim_status;
+extern int oneplus_panel_status;
 extern bool backup_dimlayer_hbm;
 extern bool HBM_flag;
 extern int dsi_panel_tx_cmd_set(struct dsi_panel *panel,
@@ -2908,6 +2908,7 @@ ssize_t notify_dim_store(struct device *dev,
 	dsi_connector = dsi_display->drm_conn;
 	mode_config = &drm_dev->mode_config;
 	sscanf(buf, "%du", &dim_status);
+
 	if (oneplus_panel_status == 0)
 		dim_status = 0;
 
@@ -2938,8 +2939,8 @@ ssize_t notify_dim_store(struct device *dev,
 	oneplus_dimlayer_hbm_enable = oneplus_dim_status != 0;
 	backup_dimlayer_hbm = oneplus_dimlayer_hbm_enable;
 	backup_dim_status = oneplus_dim_status;
-	pr_err("notify dim %d,aod = %d press= %d aod_hide =%d\n",
-		oneplus_dim_status, dsi_display->panel->aod_status, oneplus_onscreenfp_status, aod_layer_hide);
+	pr_err("notify dim %d,aod = %d press= %d aod_hide =%d oneplus_dimlayer_hbm_enable = %d\n",
+		oneplus_dim_status, dsi_display->panel->aod_status, oneplus_onscreenfp_status, aod_layer_hide, oneplus_dimlayer_hbm_enable);
 	if (oneplus_dim_status == 1 && HBM_flag) {
 		rc = dsi_panel_tx_cmd_set(dsi_display->panel, DSI_CMD_SET_HBM_ON_5);
 		if (rc) {
@@ -5073,7 +5074,7 @@ static int sde_crtc_onscreenfinger_atomic_check(struct sde_crtc_state *cstate,
 		(oneplus_dim_status == 5 || aod_layer_hide == 1)) {
 		SDE_ERROR("display  oneplus_onscreenaod_hid is 1\n");
 		oneplus_aod_hid = 1;
- 		dim_mode = 0;
+		dim_mode = 0;
 	}
 
     aod_mode = oneplus_aod_hid;
